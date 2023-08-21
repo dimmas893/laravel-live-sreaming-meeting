b82c7f42c38d6477637e2db8c573f0437feed8f2
sebelumcahahaya <anandadimmas1204@gmail.com>
2023-08-21 07:24:31 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+<p>npm install</p>
+In the .env file replace the <YOUR_METERED_DOMAIN> with your Metered Domain and <YOUR_METERED_SECRET_KEY> with your Metered Secret Key.
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\MeetingController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+// URL::forceScheme('https');
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post("/createMeeting", [MeetingController::class, 'createMeeting'])->name("createMeeting");
+
+Route::post("/validateMeeting", [MeetingController::class, 'validateMeeting'])->name("validateMeeting");
+
+Route::get("/meeting/{meetingId}", function ($meetingId) {
+    $METERED_DOMAIN = env('METERED_DOMAIN');
+    return view('meeting', [
+        'METERED_DOMAIN' => $METERED_DOMAIN,
+        'MEETING_ID' => $meetingId
+    ]);
+});
